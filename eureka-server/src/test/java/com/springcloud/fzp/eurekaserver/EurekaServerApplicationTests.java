de5df28140469e66a5405058b50c512ae4e13e38
GitHubMrChen <oschina_java@126.com>
2021-02-20 10:17:32 +0800

    add module eureka-server

---
+++ eureka-server/src/test/java/com/springcloud/fzp/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.fzp.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
