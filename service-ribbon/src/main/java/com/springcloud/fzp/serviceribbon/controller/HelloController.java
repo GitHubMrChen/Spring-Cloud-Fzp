54f5ada8d2c8eabb710ed3a3b8592140abf8b119
pdchen <oschina_java@126.com>
2021-02-22 22:47:21 +0800

    add service-ribbon

---
+++ service-ribbon/src/main/java/com/springcloud/fzp/serviceribbon/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.springcloud.fzp.serviceribbon.controller;
+
+import com.springcloud.fzp.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+
+}
