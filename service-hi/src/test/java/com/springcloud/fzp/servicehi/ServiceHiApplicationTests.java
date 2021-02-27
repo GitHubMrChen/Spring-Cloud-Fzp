05a92fd78ee41f42dc9d53d41be2965123b347cb
pdchen <oschina_java@126.com>
2021-02-27 18:00:14 +0800

    sleuth

---
+++ service-hi/src/test/java/com/springcloud/fzp/servicehi/ServiceHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.fzp.servicehi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
